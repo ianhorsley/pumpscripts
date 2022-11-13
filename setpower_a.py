775f46954a93cbd84c98c4f60b22dfbe265bbb0f
Ian Horsley <ianhorsley@cantab.net>
2022-11-13 17:20:44 +0000

    Update setpower_a.py
    add comments and relabel a variable

--- setpower_a.py
+++ setpower_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''
 Control the Brightness of LED using PWM on Raspberry Pi
-http://www.electronicwings.com
+
 '''
 
 #import RPi.GPIO as GPIO
@@ -12,7 +12,8 @@ import sys
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
 
-def get_pwm(power): #takes a percentage power and returns pwm setting. 0-100 from min to max pump speed pwm(84 - 10).
+def get_pwm(power): 
+    #takes a percentage power and returns pwm setting. 0-100 from min to max pump speed pwm (84 - 10).
     boundedpower = clamp(power, 0, 100)
     if(power != boundedpower):
         print("power request outside bounds, limited")
@@ -20,21 +21,24 @@ def get_pwm(power): #takes a percentage power and returns pwm setting. 0-100 fro
     return int(pwm)
 
 def get_pwm_max():
+    #returns pwm for max power, <10
     return 5
     
 def get_pwm_min():
+    #returns pwm for min power, 84< <92?
     return 88
 
 def get_pwm_stop():
+    #returns pwm for stop of the motor > 92
     return 98
 
-ledpin = 12                             # PWM pin connected to LED
+pwmpin = 12                             # PWM pin connected to LED
 
 #  PWM_MODE_MS = 0
 #  PWM_MODE_BAL = 1
 
 wiringpi.wiringPiSetupPhys()  # OR, using P1 header pin numbers  
-wiringpi.pinMode(ledpin,wiringpi.PWM_OUTPUT)      # pwm only works on P1 header pin 12  
+wiringpi.pinMode(pwmpin,wiringpi.PWM_OUTPUT)      # pwm only works on P1 header pin 12  
 wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
 
 # set the clock divisor to reduce the 19.2 Mhz clock
@@ -50,14 +54,14 @@ wiringpi.pwmSetClock (192)  # 19.2 Mhz divided by 3840 is 5 Khz. 192 for 100Hz 4
 pwmrange = 1000
 wiringpi.pwmSetRange (pwmrange)  # range is 2500 counts to give us half second.
 
-#wiringpi.pwmWrite(ledpin, int(pwmrange/2))    # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
+#wiringpi.pwmWrite(pwmpin, int(pwmrange/2))    # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
 
 
 try:
     power = int(sys.argv[1])
     duty = get_pwm(power)
     print('power={:d}, pwm={:d}'.format(power,duty))
-    wiringpi.pwmWrite(ledpin, int(round(pwmrange*duty/100))) #provide duty cycle in the range 0-100
+    wiringpi.pwmWrite(pwmpin, int(round(pwmrange*duty/100))) #provide duty cycle in the range 0-100
     sleep(30)
 except IndexError:
     raise SystemExit(f"Usage: {sys.argv[0]} dutycycle")
@@ -67,6 +71,6 @@ except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
     print("keyboard interrupt")
 
 #finally:
-    #wiringpi.pinMode(ledpin,wiringpi.INPUT)
-    #wiringpi.pullUpDnControl(ledpin,wiringpi.PUD_DOWN)
-#    wiringpi.pwmWrite(ledpin, 0)
+    #wiringpi.pinMode(pwmpin,wiringpi.INPUT)
+    #wiringpi.pullUpDnControl(pwmpin,wiringpi.PUD_DOWN)
+#    wiringpi.pwmWrite(pwmpin, 0)
