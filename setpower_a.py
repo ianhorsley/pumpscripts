c6be22ecc66070fe245348d15f999654b0e58313
Ian Horsley <39375170+ianhorsley@users.noreply.github.com>
2022-11-14 14:46:09 +0000

    remove duplication by refactoring

--- setpower_a.py
+++ setpower_a.py
@@ -4,10 +4,7 @@ Control the Brightness of LED using PWM on Raspberry Pi
 
 '''
 
-#import RPi.GPIO as GPIO
-import wiringpi
-from time import sleep
-import sys
+import setpwm2_a
 
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
@@ -32,45 +29,8 @@ def get_pwm_stop():
     #returns pwm for stop of the motor > 92
     return 98
 
-pwmpin = 12                             # PWM pin connected to LED
+power = int(sys.argv[1])
+duty = get_pwm(power)
+print('power={:d}, pwm={:d}'.format(power,duty))
 
-#  PWM_MODE_MS = 0
-#  PWM_MODE_BAL = 1
-
-wiringpi.wiringPiSetupPhys()  # OR, using P1 header pin numbers  
-wiringpi.pinMode(pwmpin,wiringpi.PWM_OUTPUT)      # pwm only works on P1 header pin 12  
-wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
-
-# set the clock divisor to reduce the 19.2 Mhz clock
-# to something slower, 5 Khz.
-# Range of pwmSetClock() is 2 to 4095.
-wiringpi.pwmSetClock (192)  # 19.2 Mhz divided by 3840 is 5 Khz. 192 for 100Hz 48 for 4kHz
-
-# set the PWM range which is the value for the range counter
-# which is decremented at the modified clock frequency.
-# in this case we are decrementing the range counter 5,000
-# times per second since the clock at 19.2 Mhz is being
-# divided by 3840 to give us 5 Khz.
-pwmrange = 1000
-wiringpi.pwmSetRange (pwmrange)  # range is 2500 counts to give us half second.
-
-#wiringpi.pwmWrite(pwmpin, int(pwmrange/2))    # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
-
-
-try:
-    power = int(sys.argv[1])
-    duty = get_pwm(power)
-    print('power={:d}, pwm={:d}'.format(power,duty))
-    wiringpi.pwmWrite(pwmpin, int(round(pwmrange*duty/100))) #provide duty cycle in the range 0-100
-    sleep(30)
-except IndexError:
-    raise SystemExit(f"Usage: {sys.argv[0]} dutycycle")
-except ValueError:
-    raise SystemExit(f"duty cycle must be int from 0 to 100")
-except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
-    print("keyboard interrupt")
-
-#finally:
-    #wiringpi.pinMode(pwmpin,wiringpi.INPUT)
-    #wiringpi.pullUpDnControl(pwmpin,wiringpi.PUD_DOWN)
-#    wiringpi.pwmWrite(pwmpin, 0)
+setpwm2_a.writetopwm(duty)
