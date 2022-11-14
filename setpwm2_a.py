28c844fd4b2d3a191d00208c8f2a24b767a3abc2
Ian Horsley <39375170+ianhorsley@users.noreply.github.com>
2022-11-14 14:21:31 +0000

    restructure pwm writer

--- setpwm2_a.py
+++ setpwm2_a.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 '''
-Control the Brightness of LED using PWM on Raspberry Pi
-http://www.electronicwings.com
+Control the PWM on Raspberry Pi
 '''
 
 #import RPi.GPIO as GPIO
@@ -9,43 +8,52 @@ import wiringpi
 from time import sleep
 import sys
 
-ledpin = 12                             # PWM pin connected to LED
-
-#  PWM_MODE_MS = 0
-#  PWM_MODE_BAL = 1
-
-wiringpi.wiringPiSetupPhys()  # OR, using P1 header pin numbers  
-wiringpi.pinMode(ledpin,wiringpi.PWM_OUTPUT)      # pwm only works on P1 header pin 12  
-wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
-
+pwmpin = 12         # PWM pin connected to output
 # set the clock divisor to reduce the 19.2 Mhz clock
 # to something slower, 5 Khz.
 # Range of pwmSetClock() is 2 to 4095.
-wiringpi.pwmSetClock (192)  # 19.2 Mhz divided by 3840 is 5 Khz. 192 for 100Hz 48 for 4kHz
-
+pwmclockdiv = 192   # 19.2 Mhz divided by 3840 is 5 Khz. 192 for 100Hz 48 for 4kHz
 # set the PWM range which is the value for the range counter
 # which is decremented at the modified clock frequency.
 # in this case we are decrementing the range counter 5,000
 # times per second since the clock at 19.2 Mhz is being
 # divided by 3840 to give us 5 Khz.
-pwmrange = 1000
-wiringpi.pwmSetRange (pwmrange)  # range is 2500 counts to give us half second.
+pwmrange = 1000 # range of 2500 would give us half second.
 
-#wiringpi.pwmWrite(ledpin, int(pwmrange/2))    # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
+wiringpi.wiringPiSetupPhys()  # OR, using P1 header pin numbers  
+wiringpi.pinMode(pwmpin,wiringpi.PWM_OUTPUT)      # pwm only works on P1 header pin 12  
+wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
 
+wiringpi.pwmSetClock(pwmclockdiv) #set clock divider
+wiringpi.pwmSetRange(pwmrange) #set pwm range counter
 
-try:
-    duty = int(sys.argv[1])
-    wiringpi.pwmWrite(ledpin, int(round(pwmrange*duty/100))) #provide duty cycle in the range 0-100
-    sleep(600)
-except IndexError:
-    raise SystemExit(f"Usage: {sys.argv[0]} dutycycle")
-except ValueError:
-    raise SystemExit(f"duty cycle must be int from 0 to 100")
-except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
-    print("keyboard interrupt")
+def writetopwm(pwm_level):
+    """write level to pwm pin and leave set"""
+    try:
+        duty = int(pwm_level)
+        wiringpi.pwmWrite(pwmpin, int(round(pwmrange*duty/100))) #provide duty cycle in the range 0-100
+        sleep(30)
+    except IndexError:
+        raise SystemExit(f"Usage: {sys.argv[0]} dutycycle")
+    except ValueError:
+        raise SystemExit(f"duty cycle must be int from 0 to 100")
+    except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
+        print("keyboard interrupt")
 
-#finally:
-    #wiringpi.pinMode(ledpin,wiringpi.INPUT)
-    #wiringpi.pullUpDnControl(ledpin,wiringpi.PUD_DOWN)
-#    wiringpi.pwmWrite(ledpin, 0)
+def tempwritetopwm(pwm_level, time):
+    """write level to pwm and revert after time(s)"""
+    try:
+        duty = int(pwm_level)
+        wiringpi.pwmWrite(ledpin, int(round(pwmrange*duty/100))) #provide duty cycle in the range 0-100
+        sleep(time)
+    except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
+        print("keyboard interrupt")
+    finally:
+        #do we need to write full power before disconnecting?
+        wiringpi.pinMode(ledpin,wiringpi.INPUT)
+        wiringpi.pullUpDnControl(ledpin,wiringpi.PUD_DOWN)
+
+if __name__ == "__main__":
+
+    writetopwm(sys.argv[1])
+
